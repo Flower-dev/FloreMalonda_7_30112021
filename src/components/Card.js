1558686468be5d683e350a50ebfdcc292bbd15f4
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-11-30 23:12:12 +0100

    content card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,52 @@
 class Card {
     
-    constructor() {
-   
+    constructor(name, time, ingredients, description) {
+        this.name = name
+        this.time = time
+        this.ingredients = ingredients
+        this.description = description
     }
 
 
     render() {
 
-        return (
-            `
-            
-            `
-        )
+
+        return (`
+            <div class='card_main'>
+                <div class='img_card'></div>
+                <div class='content_card'>
+                    <div class='header_card'>
+                        <div class='title_card'>
+                            <p>${this.name}</p>
+                        </div>
+                        <div class='time_card'>
+                            <p>${this.time} min</p>
+                        </div>
+                    </div>
+                    <div class='description_card'>
+                        <div class='ingredients_content'>
+                        <li><span>${this.ingredients.ingredient}</span>${this.ingredients.quantity}${this.ingredients.unit}</li>
+                        </div>
+                        <div class='description_content'>
+                            <p>${this.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
     }
 }
 
 export default Card;
+
+
+
+        // {this.ingredients}.forEach(one => {
+        //     (one.ingredient != undefined) ? ingredient = one.ingredient : ingredient = '';
+        //     (one.quantity != undefined) ? quantity = `: ${one.quantity}` : quantity = '';
+        //     (one.unit != undefined) ? unit = one.unit : unit = '';
+        //     const li = `<li><span>${ingredient}</span>${quantity}${unit}</li>`;
+        //     listElt.innerHTML += li;
+        
+        //     ingredientsArray.push(ingredient)
+        // })
--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 // components
 import NavHome from './components/NavHome.js';
-//import Card from './components/Card.js';
+import Card from './components/Card.js';
 
 
 class Index {
     constructor(){
-        // this.list = [];
-        // (async () => {
-        //   await this.loadData();
-        // })() //Immediate function
+        this.list = [];
+        (async () => {
+          await this.loadData();
+            this.renderDOM()
+        })() //Immediate function
     }
   
     /**
      * Chargement des données
      */
-    // loadData(){
-    //   return fetch('database.json')
-    //   .then((resp) => resp.json())
-    //   .then((data) => {
-    //     this.list = data.list;
-    //   })
-    //   .catch(function(error) {
-    //     console.log(error);
-    //   });
-    //    }
+    loadData(){
+      return fetch('database.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.list = data.list;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+
+    renderCards(list){
+        return list.map(function(list) {
+          const card = new Card(
+            list.name, 
+            list.time, 
+            list.ingredients,
+            list.description
+          );
+          return `<div class='card_container'>${card.render()}</div>`;
+        });
+      }
+
+    renderRecipeDOM(list){
+        const $list = document.querySelector('#cooking')
+        $list.innerHTML = this.renderCards(list).join('')
+    }
 
 
     /**
@@ -34,12 +52,13 @@ class Index {
 
         const $header = document.querySelector('#header');
         $header.innerHTML = `
-        <div class='container-profil-view'>
-            <div class='header'>
-                ${navhome.render()}
+            <div class='container-profil-view'>
+                <div class='header'>
+                    ${navhome.render()}
+                </div>
             </div>
-        </div>
         `
+        this.renderRecipeDOM(this.list);
     }
 };
 
