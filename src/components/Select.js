5ac61eebb1ce99fee518c70529147fbe6b27ac09
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-12-16 23:52:23 +0100

    test

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,24 +1,29 @@
 class Select {
-    constructor(selectList){
-        this.selectList = selectList
+    constructor(selectOptionsIngredients, selectOptionsAppliance, selectOptionsUstensils){
+        this.selectOptionsIngredients = selectOptionsIngredients
+        this.selectOptionsAppliance = selectOptionsAppliance
+        this.selectOptionsUstensils = selectOptionsUstensils
     }
 
     render(){
-
 
         return(`
             <div class='container_select'>
                 <div class='select1'>
                     <input class='ingredients' placeholder='ingredients' list='ingredients'>
-                    <datalist id='ingredients'></datalist> 
+                    <datalist id='ingredients'>${this.selectOptionsIngredients}</datalist> 
                 </div>
                 <div class='select2'>
                     <input class='appliance' placeholder='appliance' list='appliance'>
-                    <datalist id='appliance'></datalist>
+                    <datalist id='appliance'>
+                        <option value='toto'/>
+                    </datalist>
                 </div>
                 <div class='select3'>
                     <input class='ustensils' placeholder='ustensils' list='ustensils'>
-                    <datalist id='ustensils'></datalist>
+                    <datalist id='ustensils'>
+                        <option value='tutu'/>
+                    </datalist>
                 </div>
             </div>
         `)
--- src/index.js
+++ src/index.js
@@ -6,136 +6,147 @@ import Select from './components/Select.js';
 
 
 class Index {
-    constructor(){
-        this.list = [];
-        this.cards = [];
-        (async () => {
-          await this.loadData();
-            this.renderDOM()
-        })() //Immediate function
-    }
-  
-    /**
-     * Chargement des données
-     */
-    loadData(){
-      return fetch('database.json')
-      .then((resp) => resp.json())
-      .then((data) => {
-        this.list = data.list;
-      })
-      .catch(function(error) {
-        console.log(error);
+  constructor(){
+    this.list = [];
+    this.cards = [];
+    (async () => {
+      await this.loadData();
+        this.renderDOM()
+    })() //Immediate function
+  }
+
+  /**
+   * Chargement des données
+   */
+  loadData(){
+    return fetch('database.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+      this.list = data.list;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+
+// Gestion des selects
+
+  displayCards() {
+    let divCards = document.querySelector('.cards');
+    divCards.innerHTML = '';
+    this.renderCardsDOM();
+  }
+
+  // Liste des ingredients
+  selectIngredientsList(){
+    let ingredientsList = [];
+    this.list.map(function(item) {
+      let tmp = item.ingredients.map(function(subItem){
+          return subItem.ingredient
       });
+      ingredientsList = ingredientsList.concat(tmp)
+    });
+    this.ingredientsList = new Set(ingredientsList)
+    this.ingredientsList = [...ingredientsList]
+  }
+
+  selectOptionsIngredients() {
+    let optionsIngredients = '';
+    for (let i = 0; i < ingredientsList.length; i++) {
+        optionsIngredients += '<option value="' + ingredientsList[i] + '" />';
     }
+  }
 
-  // Gestion des selects
+  // Liste des appliances
+  selectAppliancesList() {
+    let appliancesList = [];
+    let arr = this.list.map(function(item) {
+          return item.appliance
+    });
+    appliancesList = appliancesList.concat(arr)
+    appliancesList = new Set(appliancesList)
+    appliancesList = [...appliancesList]
+  }
 
-     displayCards() {
-      let divCards = document.querySelector('.cards');
-      divCards.innerHTML = '';
-      this.renderCardsDOM();
+  selectOptionsAppliance() {
+    let optionsAppliance = '';
+    for (let i = 0; i < appliancesList.length; i++) {
+      optionsAppliance += '<option value="' + appliancesList[i] + '" />';
     }
+  }
+  // Liste des ustensiles 
+  selectUstensilsList(list) {
+    let ustensilsList = [];
+    let arr2 = list.map(function(item) {
+        return item.ustensils
+    });
+    ustensilsList = ustensilsList.concat(arr2)
+    ustensilsList = new Set(ustensilsList)
+    ustensilsList = [...ustensilsList]
+  }
 
-  
-    selectList(list){
-       // Liste des ingredients
-      let ingredientsList = [];
-      list.map(function(item) {
-        let tmp = item.ingredients.map(function(subItem){
-            return subItem.ingredient
-        });
-        ingredientsList = ingredientsList.concat(tmp)
-      });
-      ingredientsList = new Set(ingredientsList)
-      ingredientsList = [...ingredientsList]
-
-      let optionsIngredients = '';
-      for (let i = 0; i < ingredientsList.length; i++) {
-          optionsIngredients += '<option value="' + ingredientsList[i] + '" />';
-      }
-
-      // Liste des appliances
-      let appliancesList = [];
-      let tmp = list.map(function(item) {
-              return item.appliance
-      });
-      appliancesList = appliancesList.concat(tmp)
-      appliancesList = new Set(appliancesList)
-      appliancesList = [...appliancesList]
-
-      let optionsAppliance = '';
-      for (let i = 0; i < appliancesList.length; i++) {
-        optionsAppliance += '<option value="' + appliancesList[i] + '" />';
-      }
-
-      // Liste des ustensiles 
-  
-      let ustensilsList = [];
-      let tmp2 = this.list.map(function(item) {
-          return item.ustensils
-      });
-      ustensilsList = ustensilsList.concat(tmp2)
-      ustensilsList = new Set(ustensilsList)
-      ustensilsList = [...ustensilsList]
-
-      let optionsUstensils = '';
-      for (let i = 0; i < ustensilsList.length; i++) {
-        optionsUstensils += '<option value="' + ustensilsList[i] + '" />'
-      }
-
+  selectOptionsUstensils() {
+    let optionsUstensils = '';
+    for (let i = 0; i < ustensilsList.length; i++) {
+      optionsUstensils += '<option value="' + ustensilsList[i] + '" />'
     }
+    return optionsUstensils;
+  }
 
+  renderSearchDOM(){
+    const search = new Search();
+    const $search = document.querySelector('#search');
+    $search.innerHTML = `
+      ${search.render()}
+    `
+  }
 
-    renderSearchDOM(){
-      const search = new Search();
-      const $search = document.querySelector('#search');
-      $search.innerHTML = `
-        ${search.render()}
-      `
-    }
+  // renderSelect() {
+  //   const select = new Select((list) => this.selectList(list));
+  //   return `${select.render()}`;
+  // }
 
-    // renderSelect() {
-    //   const select = new Select((list) => this.selectList(list));
-    //   return `${select.render()}`;
-    // }
+  renderSelectDOM(){
+    const select = new Select(
+      console.log(this.selectUstensilsList),
+      this.selectOptionsIngredients,
+      this.selectOptionsAppliance,
+      this.selectOptionsUstensils
+    );
+    const $select = document.querySelector('#select');
+    $select.innerHTML = `${select.render()}`;
+  }
 
-    renderSelectDOM(){
-      const select = new Select((list) => this.selectList(list));
-      const $select = document.querySelector('#select');
-      $select.innerHTML = `${select.render()}`;
-    }
+  renderCards(list){
+    return list.map(function(list) {
+      const card = new Card(
+        list.name, 
+        list.time, 
+        list.ingredients,
+        list.description
+      );
+      return `${card.render()}`;
+    });
+  }
 
-    renderCards(list){
-      return list.map(function(list) {
-        const card = new Card(
-          list.name, 
-          list.time, 
-          list.ingredients,
-          list.description
-        );
-        return `${card.render()}`;
-      });
-    }
+  renderRecipeDOM(list){
+    const $list = document.querySelector('#cooking')
+    $list.innerHTML = this.renderCards(list).join('')
+  }
 
-    renderRecipeDOM(list){
-      const $list = document.querySelector('#cooking')
-      $list.innerHTML = this.renderCards(list).join('')
-    }
-
-    /**
-    * Création du DOM physique
-    */
-    renderDOM(){
-      const navhome = new NavHome();
-      const $header = document.querySelector('#header');
-      $header.innerHTML = `
-        ${navhome.render()}
-      `
-      this.renderSearchDOM();
-      this.renderSelectDOM(this.list);
-      this.renderRecipeDOM(this.list);
-    }
+  /**
+  * Création du DOM physique
+  */
+  renderDOM(){
+    const navhome = new NavHome();
+    const $header = document.querySelector('#header');
+    $header.innerHTML = `
+      ${navhome.render()}
+    `
+    this.renderSearchDOM();
+    this.renderSelectDOM(this.list);
+    this.renderRecipeDOM(this.list);
+  }
 };
 
 new Index();
