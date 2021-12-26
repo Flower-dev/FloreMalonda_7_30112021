244b884c0ceb67615d57203dcacfe2d16ae07b52
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2021-12-26 13:47:19 +0100

    fix(): datalist

--- src/components/Select.js
+++ src/components/Select.js
@@ -5,34 +5,18 @@ class Select {
     }
 
     
-    // typeStyle() {
-    //     this.id === 'ingredients' ? 
-    //         'btn btn-info dropdown-toggle'
-    //      : this.id === 'appliances' ? 
-    //         'btn btn-warning dropdown-toggle'
-    //      : this.id === 'ustensils' ? 
-    //         'btn btn-success dropdown-toggle'
-    // }
+    renderOption(option){
+        return `<option value="${option}"></option>`
+    }
 
 
     render(){
       
-        let value = '';
-        this.options.map(option => {
-            value += "<li>" + '<a href="#">' + option + '</a>' + "</li>"
-        })
-
         return(`
-            <div class="dropdown">
-                <button id="dLabel" class='btn btn-info dropdown-toggle' type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    ${this.id}
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2" id='${this.id}'>
-                    ${value}
-                </ul>
-            </div>
-             
+            <div>
+                <input list='${this.id}'>
+                <datalist id='${this.id}'>${this.options.map(option => this.renderOption(option))}</datalist> 
+            </div>   
         `)
     }
 }
