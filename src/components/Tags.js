63f3b3e1128a64eb24752bb8990b854d3dd7d8c9
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-01-04 22:04:14 +0100

    tags part 1

---
+++ src/components/Tags.js
@@ -0,0 +1,16 @@
+class Tags {
+
+    constructor(id, tags) {
+        this.id = id;
+        this.tags = tags;
+        this.filterTags = tags;
+    }
+
+    createTags() {
+
+    }
+
+    render(){}
+}
+
+export default Tags;
