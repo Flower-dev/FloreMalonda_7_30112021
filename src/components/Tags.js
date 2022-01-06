e4cfea079780ad1889f3635f9170e2206a09621d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-01-06 15:54:55 +0100

    test

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,35 +1,31 @@
-class Tags {
+// class Tags {
 
-    // constructor(id, tags) {
-    //     this.id = id;
-    //     this.tags = tags;
-    //     this.filterTags = tags
-    // }
+//     constructor(id, tag, filterRecipes) {
+//         this.id = id;
+//         this.tag = tag;
+//         this.filterTags = tags;
+//         this.filterRecipes = filterRecipes;
+//     }
+
+//     createTag() {
+//         const parser = new DOMParser()
+//         let dom = 
+//     }
 
    
+//     render(){
+//         let ul= '<ul>';
+//         for (let i = 0; i<this.tags.length; i++){
+//             const li =`
+//                 <li>
+//                     <a href='index.html?id=${this.tags[i]}' data-filter='${this.tags[i]}'>#${this.tags[i]}</a>
+//                 </li>
+//                 `
+//             ul += li   
+//         }
+//         ul += '</ul>'
+//         return ul;
+//     }
+// }
 
-
-    // // passer en paramètres les tableaux :
-    // // this.ingredients = [];
-	// //	this.ustensils = [];
-	// //	this.appliances = [];
-
-    // // faire comme le composant select un truc gérérique (3 render)
-
-
-    // render(){
-    //     let ul= '<ul>';
-    //     for (let i = 0; i<this.tags.length; i++){
-    //         const li =`
-    //             <li>
-    //                 <a href='index.html?id=${this.tags[i]}' data-filter='${this.tags[i]}'>#${this.tags[i]}</a>
-    //             </li>
-    //             `
-    //         ul += li   
-    //     }
-    //     ul += '</ul>'
-    //     return ul;
-    // }
-}
-
-export default Tags;
+// export default Tags;
